9c96e7d29c8c9634a8b8ded4ccea49dc6d6e9e6e
Pablosancval <pablosancval@gmail.com>
2022-01-13 18:44:41 +0100

    Exercise 2

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
@@ -10,7 +12,10 @@ import org.springframework.data.repository.CrudRepository;
 public interface ProductRepository extends CrudRepository<Product, Integer>{
 
     List<Product> findAll();
-    List<ProductType> findAllProductTypes();
+    
+	@Query("SELECT prodtype FROM ProductType prodtype ORDER BY prodtype.name")
+    List<ProductType> findAllProductTypes() throws DataAccessException;
+
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,11 +1,12 @@
 package org.springframework.samples.petclinic.product;
 
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductType {
-    Integer id;
-    String name;
+public class ProductType extends NamedEntity{
+
 }
